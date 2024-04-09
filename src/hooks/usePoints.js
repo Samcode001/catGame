1effb42adb83c9c2ef1d76ac73b509f37c975952
Samcode001 <93057570+Samcode001@users.noreply.github.com>
2024-04-09 10:16:49 +0530

    bug fixed

--- src/hooks/usePoints.js
+++ src/hooks/usePoints.js
@@ -2,26 +2,6 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 const usePoints = () => {
-  // const getPoint = async () => {
-  //   try {
-  //     const res = await axios.post(
-  //       "http://localhost:8080/findPointsByUsername",
-  //       {
-  //         username: localStorage.getItem("player"),
-  //       }
-  //     );
-
-  //     const value = res.data["sam"];
-  //     setUserPoints(value);
-  //   } catch (error) {
-  //     console.error("Error fetching user points:", error);
-  //     throw error; // Re-throw the error to handle it elsewhere, if necessary
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   getPoint();
-  // }, []);
 
   const hanldePoint = async (points) => {
     const res = await axios.post("http://localhost:8080/addpoint", {
